281be5513c6905c6ee3ce193778ff48d422a371c
Jossyelmos <jossyelmos@yahoo.com>
2026-01-15 14:30:51 +0100

    database

---
+++ database/assignment2.sql
@@ -0,0 +1,36 @@
+-- Task 1
+INSERT INTO public.account  
+	(account_firstname, 
+	account_lastname,
+	account_email,
+	account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- Task 2
+UPDATE public.account
+	SET account_type = 'Admin'
+	WHERE account_id = 1;
+
+-- Task 3
+DELETE FROM public.account
+	WHERE account_id = 1;
+
+
+-- Task 4
+UPDATE public.inventory
+	SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+	WHERE inv_model = 'Hummer';
+
+
+-- Task 5
+SELECT inv_make, inv_model, classification_name 
+FROM public.inventory i
+	INNER JOIN public.classification c
+	ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+	
+
+-- Task 6
+UPDATE public.inventory
+	SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+		inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
